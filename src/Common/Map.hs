b3e9896f7dc3c6bed3a7479f9f7f5af2c5bcc841
Boris Skert <boris.skert@gmail.com>
2022-12-10 11:25:30 +0100

    Remove redundant code

--- src/Common/Map.hs
+++
@@ -1,6 +0,0 @@
-module Common.Map (fromElems) where
-
-import Data.Map (Map, fromList)
-
-fromElems :: (Ord k) => (a -> k) -> [a] -> Map k a
-fromElems key = fromList . map (\x -> (key x, x))
--- src/Common/Tree.hs
+++ src/Common/Tree.hs
@@ -11,7 +11,6 @@ module Common.Tree
     subtree,
     subtrees,
     fromList,
-    isLeaf,
   )
 where
 
