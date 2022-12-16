feb9b6aff1c6d679c104d29d176dd2f7e04f8ea5
Boris Skert <boris.skert@gmail.com>
2022-12-16 07:54:21 +0100

    Parse MonkeyIds

--- src/Common/Regex.hs
+++ src/Common/Regex.hs
@@ -1,13 +1,28 @@
-module Common.Regex (parseGroups, parseMatches, isMatch, re, Regex) where
+module Common.Regex (parseGroups, tryParseGroups, parseGroupsFirstMatch, parseMatches, isMatch, re, Regex) where
 
+import Data.Maybe (mapMaybe)
 import Text.Regex.PCRE.Heavy
 
 parseGroups :: Regex -> String -> [String]
 parseGroups pattern input
   | null parsed = error ("Nothing parsed in '" ++ input ++ "'")
-  | otherwise = snd . head . scan pattern $ input
+  | otherwise = snd . head $ parsed
   where
     parsed = scan pattern $ input
+
+tryParseGroups :: Regex -> String -> Maybe [String]
+tryParseGroups pattern input
+  | null parsed = Nothing
+  | otherwise = Just . snd . head $ parsed
+  where
+    parsed = scan pattern $ input
+
+parseGroupsFirstMatch :: [Regex] -> String -> [String]
+parseGroupsFirstMatch patterns input
+  | null parsed = error ("Nothing parsed in '" ++ input ++ "'")
+  | otherwise = head parsed
+  where
+    parsed = filter (not . null) . mapMaybe (`tryParseGroups` input) $ patterns
 
 parseMatches :: Regex -> String -> [String]
 parseMatches pattern input = map fst . scan pattern $ input
---
+++ src/Day11/MonkeyId.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Day11.MonkeyId (MonkeyId, from) where
+
+import Common.Regex
+import Data.Maybe (catMaybes, mapMaybe)
+
+newtype MonkeyId = MonkeyId Int deriving (Eq, Show)
+
+from :: Int -> MonkeyId
+from = MonkeyId
+
+instance Read MonkeyId where
+  readsPrec _ input = [(readFrom input, [])]
+
+headLinePattern :: Regex
+headLinePattern = [re|Monkey ([0-9]+):|]
+
+throwPattern :: Regex
+throwPattern = [re|throw to monkey ([0-9]+)|]
+
+readFrom :: String -> MonkeyId
+readFrom input = MonkeyId . read . head . (`parseGroupsFirstMatch` input) $ [headLinePattern, throwPattern]
