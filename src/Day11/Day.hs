90464dbee4c2869d030f34eafd60f69026ccccec
Boris Skert <boris.skert@gmail.com>
2022-12-17 14:37:51 +0100

    Solve part 1

---
+++ src/Day11/Day.hs
@@ -0,0 +1,19 @@
+module Day11.Day (day11part1) where
+
+-- https://adventofcode.com/2022/day/11
+
+import Common.Day
+import Common.TextFile (readFileContents)
+import Day11.MonkeyInTheMiddle
+
+day11part1 :: Day
+day11part1 =
+  Day
+    { name = "day11part1",
+      friendlyName = "Day 11 Part 1",
+      isDefault = True,
+      run = do
+        input <- readFileContents "puzzleinput/day11.txt"
+        let result = monkeyLevel input
+        print result
+    }
