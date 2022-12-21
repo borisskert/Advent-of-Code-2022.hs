5c37768cc692c823e691efe1615aa98ecf4a4e7a
Boris Skert <boris.skert@gmail.com>
2022-12-21 15:11:02 +0100

    Solve part 1

--- src/Day14/RegolithReservoir.hs
+++ src/Day14/RegolithReservoir.hs
@@ -1,10 +1,12 @@
 module Day14.RegolithReservoir (sandUnits, showReservoir) where
 
 import Day14.Reservoir (empty, insertScans)
+import Day14.Simulation (dropAll, from)
+import qualified Day14.Simulation as Simulation (sandUnits)
 
 -- How many units of sand come to rest before sand starts flowing into the abyss below?
 sandUnits :: String -> Int
-sandUnits = undefined
+sandUnits = length . Simulation.sandUnits . dropAll . from . (`insertScans` empty) . read
 
 showReservoir :: String -> String
-showReservoir = show . (`insertScans` empty) . read
+showReservoir = show .  dropAll . from . (`insertScans` empty) . read
